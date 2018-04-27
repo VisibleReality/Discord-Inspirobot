cc6e27556741553b4add337b7134f48086ca685a
VisibleReality <visible.visiblebanana@alexandermitchell.id.au>
2018-04-27 21:21:20 +1000

    Added some extra comments and a few extra things.

--- main.py
+++ main.py
@@ -11,23 +11,29 @@ import io
 userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
 prefix = "!"
 token = "<token>"
+apiUrl="http://inspirobot.me/api?generate=true"
+fileUploadName = "inspirobot.jpg"
 
 # Defines the bot which allows us to create commands
 bot = commands.Bot(command_prefix=prefix, description="Inspirobot.me")
 
+# Sets the bot's presence to indicate the command to use, and prints some text to indicate that it is ready.
 @bot.listen()
 async def on_ready():
 	print("Bot ready")
 	print(bot.user.name, "("+str(bot.user.id)+")")
 	await bot.change_presence(activity=discord.Game(name='!inspire'))
 
+# Defines the !inspire command which allows you to request inspirational images on demand.
 @bot.command()
 async def inspire(ctx):
-	path_response = urllib.request.urlopen(urllib.request.Request("http://inspirobot.me/api?generate=true",headers={"User-Agent":userAgent}))
+	path_response = urllib.request.urlopen(urllib.request.Request(apiUrl,headers={"User-Agent":userAgent}))
 	image = urllib.request.urlopen(urllib.request.Request(path_response.read().decode("utf-8"),headers={"User-Agent":UserAgent}))
 	data = io.BytesIO(image.read())
-	await ctx.send(file=discord.File(data, "inspirobot.jpg"))
+	await ctx.send(file=discord.File(data, fileUploadName))
 
+# Removes the auto generated help command as it's not required
 bot.remove_command('help')
 
+# Starts running the bot
 bot.run("<token>")
